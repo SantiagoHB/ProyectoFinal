ffa21767352157b4e7dc5c8eb9e43b837111dd0f
Santiago Hernández <73314160+SantiagoHB@users.noreply.github.com>
2022-11-14 18:52:47 -0500

    Update Cliente.java

--- src/Clases/Cliente.java
+++ src/Clases/Cliente.java
@@ -3,9 +3,9 @@ package Clases;
 import java.util.Date;
 
 public class Cliente {
-short Cedula;
-String Nombre;
-String Email;
-Date FechaNacimiento;
+    short Cedula;
+    String Nombre;
+    String Email;
+    Date FechaNacimiento;
 
 }
